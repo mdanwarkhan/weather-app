a9df3a107fe33f40107a875d034bc502aa3a87a5
anwar khan <anwarkhan9292@gmail.com>
2020-04-02 00:00:10 +0530

    removed console

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client side javscript')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#msg1')
